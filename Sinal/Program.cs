5f4e8e8d3f18d0db30a547afa1079608e5257b3d
MWLeonel <wmleonel6@gmail.com>
2020-10-01 12:43:31 -0300

    Descobrir se é positivo, negativo ou zero

---
+++ Sinal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            Console.Write("Digite um número: ");
+            valor = int.Parse(Console.ReadLine());
+
+            if (valor > 0)
+            {
+                Console.WriteLine("Você digitou um número positivo!");
+            }
+            else if (valor < 0)
+            {
+                Console.WriteLine("Você digitou um número negativo");
+            }
+            else
+            {
+                Console.WriteLine("Você digitou zero");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
